9ee6b1dabbccf5d5e40eda3f6f09f43e1c747cdd
Phillip Gutowski <phillip.gutowski@tapjoy.com>
2016-05-09 11:50:31 -0400

    remove refills from the parent directory

--- refills.js
+++
@@ -1,7 +0,0 @@
-var expanderContainers = document.getElementsByClassNames('')
-
-$(document).ready(function() {
-  $('.expander-trigger').click(function(){
-    $(this).toggleClass("expander-hidden");
-  });
-});
